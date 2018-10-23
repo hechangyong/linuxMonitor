1f2441ed4f96740059faee4b989a583735c5ed67
hecy <hechangyong@aliyun.com>
2018-10-23 12:01:26 +0800

    添加监控服务

---
+++ src/main/java/com/ms/linuxMonitor/LinuxMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.ms.linuxMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxMonitorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ms/linuxMonitor/LinuxMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ms.linuxMonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinuxMonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
